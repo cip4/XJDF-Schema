03451dbdea9a465e59c5d0f8c319f6081a5c1ee1
Michel Hartmann <michel.hartmann@flyeralarm.com>
2019-03-27 13:25:41 +0100

    Validate against the schema defined in this repo

--- src/test/groovy/org/cip4/xjdf/samples/XjdfSchemaValidation.groovy
+++ src/test/groovy/org/cip4/xjdf/samples/XjdfSchemaValidation.groovy
@@ -8,12 +8,17 @@ import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
 import groovy.io.FileType
 
+import javax.xml.XMLConstants
 import javax.xml.bind.ValidationException
+import javax.xml.transform.stream.StreamSource
+import javax.xml.validation.SchemaFactory
+import javax.xml.validation.Validator
 
 class XjdfSchemaValidation {
 
-    private static XJdfValidator validator;
     private static Locale defaultLocale
+    private static File xsdUri = new File("./xjdf.xsd");
+    private static Validator validator
 
     static Collection<Arguments> data() {
         List<Arguments> files = new ArrayList<>();
@@ -29,7 +34,12 @@ class XjdfSchemaValidation {
     public static void setUp() throws Exception {
         defaultLocale = Locale.getDefault();
         Locale.setDefault(Locale.US)
-        validator = new XJdfValidator();
+
+        xsdUri.withInputStream { xsd ->
+            validator = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
+                    .newSchema(new StreamSource(xsd))
+                    .newValidator()
+        }
     }
 
     @AfterAll
@@ -41,7 +51,9 @@ class XjdfSchemaValidation {
     @MethodSource("data")
     public void testInvalidPackages(File xjdfFile) {
         try {
-            validator.validate(xjdfFile.newInputStream())
+            xjdfFile.withInputStream { xml ->
+                validator.validate(new StreamSource(xml))
+            }
         } catch (ValidationException e) {
             throw new AssertionError(
                     String.format(
